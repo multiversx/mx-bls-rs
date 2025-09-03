c2e39a8cbf875495e1a378de7f92d79df61c67bd
BiancaIalangi <bianca.ialangi@multiversx.com>
2025-09-03 20:35:52 +0300

    Add execs

--- build.rs
+++ build.rs
@@ -1,8 +1,33 @@
-// TODO make compatible for all platforms
-// depends on the system: arm or amd || linux or macos
 fn main() {
-    // current implementation is for linux amd64
-    println!("cargo:rustc-link-search=native=.");
-    println!("cargo:rustc-link-lib=static=bls384_256");
-    println!("cargo:rustc-link-lib=stdc++");
+    let target = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    match (target.as_str(), os.as_str()) {
+        ("x86_64", "linux") => {
+            println!("cargo:rustc-link-search=native=./executors");
+            println!("cargo:rustc-link-lib=static=bls384_256_amd_linux");
+            println!("cargo:rustc-link-lib=stdc++");
+        }
+        ("aarch64", "linux") | ("arm", "linux") => {
+            println!("cargo:rustc-link-search=native=./executors");
+            println!("cargo:rustc-link-lib=static=bls384_256_arm_linux");
+            println!("cargo:rustc-link-lib=stdc++");
+        }
+        ("x86_64", "macos") => {
+            println!("cargo:rustc-link-search=native=./executors");
+            println!("cargo:rustc-link-lib=static=bls384_256_amd_macos");
+            println!("cargo:rustc-link-lib=c++");
+        }
+        ("aarch64", "macos") | ("arm", "macos") => {
+            println!("cargo:rustc-link-search=native=./executors");
+            println!("cargo:rustc-link-lib=static=bls384_256_arm_macos");
+            println!("cargo:rustc-link-lib=c++");
+        }
+        (_, "windows") => {
+            println!("cargo:rustc-link-search=native=./executors");
+            println!("cargo:rustc-link-lib=static=bls384_256_arm_macos");
+            println!("cargo:rustc-link-lib=stdc++");
+        }
+        _ => panic!("Unsupported target: {os}-{target}"),
+    }
 }
