73aa32ed83fc5d102229a1815ae62cf63ab3b7f8
Andrei Marinica <andrei.marinica@multiversx.com>
2025-09-04 15:12:03 +0300

    extern C unsafe fn

--- src/bls_api.rs
+++ src/bls_api.rs
@@ -4,30 +4,40 @@ use crate::gt::GT;
 use crate::secret_key::SecretKey;
 
 unsafe extern "C" {
-    pub fn blsInit(curve: usize, compiledTimeVar: usize) -> i32;
+    pub unsafe fn blsInit(curve: usize, compiledTimeVar: usize) -> i32;
 
-    pub fn mclBnFr_isZero(secret_key: *const SecretKey) -> i32;
-    pub fn mclBn_getFrByteSize() -> usize;
-    pub fn mclBn_getFpByteSize() -> usize;
-    pub fn mclBnG2_setStr(x: *mut G2, buf: *const u8, buf_size: usize, io_mode: i32) -> i32;
-    pub fn mclBnG2_deserialize(x: *mut G2, buf: *const u8, buf_size: usize) -> usize;
-    pub fn mclBnG2_isZero(x: *const G2) -> u8;
-    pub fn mclBnG2_isValid(x: *const G2) -> u8;
+    pub unsafe fn mclBnFr_isZero(secret_key: *const SecretKey) -> i32;
+    pub unsafe fn mclBn_getFrByteSize() -> usize;
+    pub unsafe fn mclBn_getFpByteSize() -> usize;
+    pub unsafe fn mclBnG2_setStr(x: *mut G2, buf: *const u8, buf_size: usize, io_mode: i32) -> i32;
+    pub unsafe fn mclBnG2_deserialize(x: *mut G2, buf: *const u8, buf_size: usize) -> usize;
+    pub unsafe fn mclBnG2_isZero(x: *const G2) -> u8;
+    pub unsafe fn mclBnG2_isValid(x: *const G2) -> u8;
 
-    pub fn mclBnG1_isZero(x: *const G1) -> u8;
-    pub fn mclBnG1_isValid(x: *const G1) -> u8;
+    pub unsafe fn mclBnG1_isZero(x: *const G1) -> u8;
+    pub unsafe fn mclBnG1_isValid(x: *const G1) -> u8;
 
-    pub fn blsSecretKeySetByCSPRNG(x: *mut SecretKey);
-    pub fn blsSecretKeySetHexStr(x: *mut SecretKey, buf: *const u8, buf_size: usize) -> i32;
-    pub fn blsGetPublicKey(y: *mut G2, x: *const SecretKey);
-    pub fn blsSignatureVerifyOrder(do_verify: i32);
-    pub fn blsSignatureIsValidOrder(sig: *const G1) -> i32;
-    pub fn blsPublicKeyVerifyOrder(do_verify: i32);
-    pub fn blsPublicKeyIsValidOrder(public_key: *const G2) -> i32;
+    pub unsafe fn blsSecretKeySetByCSPRNG(x: *mut SecretKey);
+    pub unsafe fn blsSecretKeySetHexStr(x: *mut SecretKey, buf: *const u8, buf_size: usize) -> i32;
+    pub unsafe fn blsGetPublicKey(y: *mut G2, x: *const SecretKey);
+    pub unsafe fn blsSignatureVerifyOrder(do_verify: i32);
+    pub unsafe fn blsSignatureIsValidOrder(sig: *const G1) -> i32;
+    pub unsafe fn blsPublicKeyVerifyOrder(do_verify: i32);
+    pub unsafe fn blsPublicKeyIsValidOrder(public_key: *const G2) -> i32;
 
-    pub fn blsSign(sig: *mut G1, secret_key: *const SecretKey, msg: *const u8, msg_len: usize);
-    pub fn blsVerify(sig: *const G1, public_key: *const G2, msg: *const u8, msg_len: usize) -> i32;
-    pub fn blsFastAggregateVerify(
+    pub unsafe fn blsSign(
+        sig: *mut G1,
+        secret_key: *const SecretKey,
+        msg: *const u8,
+        msg_len: usize,
+    );
+    pub unsafe fn blsVerify(
+        sig: *const G1,
+        public_key: *const G2,
+        msg: *const u8,
+        msg_len: usize,
+    ) -> i32;
+    pub unsafe fn blsFastAggregateVerify(
         sig: *const G1,
         public_key: *const G2,
         public_keys_len: usize,
@@ -35,26 +45,34 @@ unsafe extern "C" {
         msg_len: usize,
     ) -> i32;
 
-    pub fn blsAggregateSignature(
+    pub unsafe fn blsAggregateSignature(
         aggregate_sig: *mut G1,
         signature: *const G1,
         signatures_len: usize,
     );
 
-    pub fn blsSecretKeyIsEqual(lhs: *const SecretKey, rhs: *const SecretKey) -> i32;
-    pub fn blsPublicKeyIsEqual(lhs: *const G2, rhs: *const G2) -> i32;
-    pub fn blsSignatureIsEqual(lhs: *const G1, rhs: *const G1) -> i32;
+    pub unsafe fn blsSecretKeyIsEqual(lhs: *const SecretKey, rhs: *const SecretKey) -> i32;
+    pub unsafe fn blsPublicKeyIsEqual(lhs: *const G2, rhs: *const G2) -> i32;
+    pub unsafe fn blsSignatureIsEqual(lhs: *const G1, rhs: *const G1) -> i32;
 
-    pub fn blsSecretKeySerialize(buf: *mut u8, max_buf_len: usize, x: *const SecretKey) -> usize;
-    pub fn blsPublicKeySerialize(buf: *mut u8, max_buf_len: usize, x: *const G2) -> usize;
-    pub fn blsSignatureSerialize(buf: *mut u8, max_buf_len: usize, x: *const G1) -> usize;
+    pub unsafe fn blsSecretKeySerialize(
+        buf: *mut u8,
+        max_buf_len: usize,
+        x: *const SecretKey,
+    ) -> usize;
+    pub unsafe fn blsPublicKeySerialize(buf: *mut u8, max_buf_len: usize, x: *const G2) -> usize;
+    pub unsafe fn blsSignatureSerialize(buf: *mut u8, max_buf_len: usize, x: *const G1) -> usize;
 
-    pub fn blsSecretKeyDeserialize(x: *mut SecretKey, buf: *const u8, buf_len: usize) -> usize;
-    pub fn blsPublicKeyDeserialize(x: *mut G2, buf: *const u8, buf_len: usize) -> usize;
-    pub fn blsSignatureDeserialize(x: *mut G1, buf: *const u8, buf_len: usize) -> usize;
+    pub unsafe fn blsSecretKeyDeserialize(
+        x: *mut SecretKey,
+        buf: *const u8,
+        buf_len: usize,
+    ) -> usize;
+    pub unsafe fn blsPublicKeyDeserialize(x: *mut G2, buf: *const u8, buf_len: usize) -> usize;
+    pub unsafe fn blsSignatureDeserialize(x: *mut G1, buf: *const u8, buf_len: usize) -> usize;
 
-    pub fn blsPublicKeyAdd(public_key_1: *mut G2, public_key_2: *const G2);
-    pub fn blsSignatureAdd(signature_1: *mut G1, signature_2: *const G1);
+    pub unsafe fn blsPublicKeyAdd(public_key_1: *mut G2, public_key_2: *const G2);
+    pub unsafe fn blsSignatureAdd(signature_1: *mut G1, signature_2: *const G1);
 
-    pub fn mclBnGT_isEqual(lhs: *const GT, rhs: *const GT) -> i32;
+    pub unsafe fn mclBnGT_isEqual(lhs: *const GT, rhs: *const GT) -> i32;
 }
