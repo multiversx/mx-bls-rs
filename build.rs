5c9edf9642bf1e4febf57a3e7cc0d34dde3d2871
Andrei Marinica <andrei.marinica@multiversx.com>
2026-05-21 01:21:35 +0300

    windows build fix, CI refactor

--- build.rs
+++ build.rs
@@ -26,6 +26,10 @@ fn main() {
         ("x86_64", "windows") => {
             println!("cargo:rustc-link-search=native=./executors");
             println!("cargo:rustc-link-lib=static=bls384_256_amd_windows");
+            let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
+            if env == "gnu" {
+                println!("cargo:rustc-link-lib=stdc++");
+            }
         }
         _ => panic!("Unsupported target: {os}-{target}"),
     }
