ef2fad7e123df27ab8ef2ff91b2b67231a2e7dc3
Andrei Marinica <andrei.marinica@multiversx.com>
2026-05-21 17:50:53 +0300

    windows build fix

--- build.rs
+++ build.rs
@@ -24,13 +24,18 @@ fn main() {
             println!("cargo:rustc-link-lib=c++");
         }
         ("x86_64", "windows") => {
+            let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
+            if env == "msvc" {
+                panic!(
+                    "The x86_64-pc-windows-msvc target is not supported: only a MinGW/GNU-compatible \
+                     static archive is provided. Use the x86_64-pc-windows-gnu target instead, or \
+                     supply an MSVC-compatible import/static library."
+                );
+            }
             println!("cargo:rustc-link-search=native=./executors");
             println!("cargo:rustc-link-lib=static=bls384_256_amd_windows");
             println!("cargo:rustc-link-lib=advapi32");
-            let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
-            if env == "gnu" {
-                println!("cargo:rustc-link-lib=stdc++");
-            }
+            println!("cargo:rustc-link-lib=stdc++");
         }
         _ => panic!("Unsupported target: {os}-{target}"),
     }
